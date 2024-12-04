3671ccdc29021114b210af8dfd3f18f2d6ad4e55
wusabbrin <40818275+wusabbrin@users.noreply.github.com>
2024-12-03 19:54:40 -0600

    Add files via upload

--- labrad_manager.bat
+++ labrad_manager.bat
@@ -1,6 +1,7 @@
 :: This file may have to be tweaked for your specific PC. Just consider it a starting point. 
-start cmd /k ""C:\Users\Saroj Chand\Documents\dioptric\manager\scalabrad-0.8.3\bin\labrad.bat""
-start cmd /k ""C:\Users\Saroj Chand\Documents\dioptric\manager\scalabrad-web-server-2.0.5\bin\labrad-web.bat""
-start chrome /new-window http://localhost:7667
-call "C:\Users\Saroj Chand\miniconda3\Scripts\activate.bat" dioptric
-call python -m labrad.node -u "" -w ""
+@REM start cmd /k manager\scalabrad-0.8.3\bin\labrad.bat
+start cmd /k "C:\Users\choyl\ChoyDioptric\manager\scalabrad-0.8.3\bin\labrad.bat"
+start cmd /k manager\scalabrad-web-server-2.0.5\bin\labrad-web.bat
+start "" "C:\Users\choyl\ChoyDioptric\chrome\chrome.exe" /new-window http://localhost:7667
+call "C:\ProgramData\anaconda3\Scripts\activate.bat" dioptric
+call python -m labrad.node
